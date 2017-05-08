4a01d86d508ab672dc426a0c1b9f68176d6fcfac
Adan206 <adan@Adans-MacBook-Pro.local>
2017-05-07 23:03:39 -0700

    changes

---
+++ app/scripts/services/current.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularAppApp.current
+ * @description
+ * # current
+ * Factory in the angularAppApp.
+ */
+ angular.module('WeatherZone')
+   .factory('current', function ($resource) {
+     // Service logic
+     // ...
+
+     // Public API here
+     return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&mode=json&APPID=c6652cd966bc34851add11456f4da83d', {}, {
+      find: {
+        method: 'GET',
+        params: {
+          query: 'seattle'
+        },
+        isArray: false
+      }
+    });
+  });
